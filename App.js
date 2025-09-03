8327842b43c2ba49ec0531bf9bac22cbeaf7b17d
Falkun <66378650+FelipeDinnouti@users.noreply.github.com>
2025-09-03 10:13:37 -0300

    Simple FlatList implementation

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SectionList, Dimensions } from 'react-native';
+import { FlatListItem } from 'src/FlatListItem.js'
+
+const kExampleFlatListData = [
+  {
+    title: 'First Item',
+    subtitle: 'First Subtitle',
+    color: "#D287F9"
+  },
+  {
+    title: 'Second Item',
+    subtitle: 'Second Subtitle',
+    color: "#F56255",
+  },
+  {
+    title: 'Third Item',
+    subtitle: 'Third Subtitle',
+    color: "#7298FF"
+  },
+];
+
+const kWindowWidth = Dimensions.get('window').width;
+
+function createColorStyle(color) {
+  return {
+    backgroundColor: color, 
+    borderTopLeftRadius: 8,
+    borderBottomLeftRadius: 8,
+    flex: 0.1,
+    flexGrow: 0,
+    flexShrink: 0,
+  }
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bom dia, Aluno!</Text>
+      <FlatList
+        data={kExampleFlatListData}
+        renderItem={({item}) => <FlatlistItem title={item.title} subtitle={item.subtitle} color={item.color}/>}
+        />
     </View>
   );
 }
@@ -17,4 +51,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    marginVertical: 40,
+    fontSize: 32
+  },
+  item_title: {
+    fontSize: 20
+  },
+  item_subtitle: {
+    fontSize: 12
+  },
+  flatlist_item: {
+    width: kWindowWidth*0.85,
+    marginBottom: 10,
+    backgroundColor: '#f0f2f3',
+
+    borderRadius: 8,
+
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  flatlist_text_container: {
+    marginVertical: 8, 
+    display: 'flex',
+    flex: 1,
+    paddingHorizontal: 20
+  }
+  
 });
---
+++ src/FlatListItem.js
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, View, FlatList, SectionList, Dimensions } from 'react-native';
+
+const kWindowWidth = Dimensions.get('window').width;
+
+function createColorStyle(color) {
+    return {
+      backgroundColor: color, 
+      borderTopLeftRadius: 8,
+      borderBottomLeftRadius: 8,
+      flex: 0.1,
+      flexGrow: 0,
+      flexShrink: 0,
+    }
+  }
+  
+export default FlatlistItem = ({title, subtitle, color}) => (
+    <View style={styles.flatlist_item}>
+      <View style={createColorStyle(color)}>
+      </View>
+  
+      <View style={styles.flatlist_text_container}>
+        <Text style={styles.item_title}>{title}</Text>
+        <Text style={styles.item_subtitle}>{subtitle}</Text>
+      </View>
+    </View>
+);
+
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+title: {
+    marginVertical: 40,
+    fontSize: 32
+},
+item_title: {
+    fontSize: 20
+},
+item_subtitle: {
+    fontSize: 12
+},
+flatlist_item: {
+    width: kWindowWidth*0.85,
+    marginBottom: 10,
+    backgroundColor: '#f0f2f3',
+
+    borderRadius: 8,
+
+    display: 'flex',
+    flexDirection: 'row'
+},
+flatlist_text_container: {
+    marginVertical: 8, 
+    display: 'flex',
+    flex: 1,
+    paddingHorizontal: 20
+}
+
+});
